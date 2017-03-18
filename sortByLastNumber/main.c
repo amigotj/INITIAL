05fdb1a9426fbd63fe03e118ca8ad3a4aed0d932
amigotj <edroid@mail.ru>
2017-03-18 18:27:35 +0200

    123
    123

--- sortByLastNumber/main.c
+++ sortByLastNumber/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+int sortBubble (int *, int);
 
 int main(int argc, char *argv[])
 {
-	printf("Hello C-Free!\n");
+	printf("Input count of elements and elements of massive\n");
+	int n;
+	scanf("%d",&n);
+	int i;
+	int A[n];
+	for (i=0; i<n; i++)
+	{
+		scanf("%d",&A[i]);
+	}
+	sortBubble(*A,n);
 	return 0;
+}
+
+int sortBubble (int A*, int n)
+{
+	printf("")
 }
